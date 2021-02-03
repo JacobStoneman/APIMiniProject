12b9d4f5551d7d1c5684186c790c1a27c5411425
zakkazmanli <zakariyyaakazmanli@gmail.com>
2021-02-03 16:55:38 +0000

    Add initial create calls

---
+++ APIMiniProject/APIMiniProject/Services/Services/ProjectCreateService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIMiniProject
+{
+    public class ProjectCreateService
+    {
+        public ProjectCreateCallManager ProjectCreateCallManager;
+        public string ResponseAsString;
+        public ProjectCreateService(ProjectCreateCallManager projectCreateCallManager)
+        {
+            ProjectCreateCallManager = projectCreateCallManager;
+        }
+
+        public void CreateProjectWithName(string name)
+        {
+            var response = ProjectCreateCallManager.CreateProject(name);
+        }
+    }
+}
---
+++ APIMiniProject/APIMiniProject/Tests/ProjectCreateTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using RestSharp;
+
+namespace APIMiniProject
+{
+    public class ProjectCreateTests
+    {
+        [Test]
+        public void CreateCallSuccessfullyCreates()
+        {
+            ProjectCreateService createService = new ProjectCreateService(new ProjectCreateCallManager(new RestClient(AppConfigReader.BaseUrl)));
+            createService.CreateProjectWithName("Testing");
+            Assert.That(createService.ResponseAsString, Is.True);
+        }
+    }
+}
