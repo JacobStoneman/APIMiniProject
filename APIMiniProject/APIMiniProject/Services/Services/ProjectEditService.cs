5d7838f6607001edb670a1b112520abb794c7a2f
Rob Bentley <robertbentley96@hotmail.co.uk>
2021-02-03 15:53:04 +0000

    Added Project Edit Functionality

---
+++ APIMiniProject/APIMiniProject/Services/Services/ProjectEditService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace APIMiniProject
+{
+	public class ProjectEditService
+	{
+		public ProjectEditCallManager ProjectEditCallManager { get; set; }
+		public ProjectEditService(IRestClient client)
+		{
+			ProjectEditCallManager = new ProjectEditCallManager(client);
+		}
+
+		public void EditProject(int id, string name, int color = -1)
+		{
+			JObject jsonbody = new JObject(new JProperty("name", name));
+			if (color > -1)
+			{
+				jsonbody.Add("color", color);
+			}
+			string result = ProjectEditCallManager.PostProject(id, jsonbody);
+		}
+	}
+}
