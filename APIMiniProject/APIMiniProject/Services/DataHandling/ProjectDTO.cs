a3cea1b493800ef04b4a413a1d19523e23643809
zakkazmanli <zakariyyaakazmanli@gmail.com>
2021-02-03 14:11:47 +0000

    Update ProjectDTOs

---
+++ APIMiniProject/APIMiniProject/Services/DataHandling/ProjectDTO.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIMiniProject.Services.DataHandling
+{
+    public class ProjectDTO
+    {
+        public ProjectDTO Project { get; set; }
+        public void DeserialiseRates(string projectJsonString)
+        {
+            Project = JsonConvert.DeserializeObject<ProjectDTO>(projectJsonString);
+        }
+    }
+}
---
+++ APIMiniProject/APIMiniProject/Services/DataHandling/ProjectListDTO.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIMiniProject.Services.DataHandling
+{
+	public class ProjectListDTO
+	{
+        public ProjectListDTO ProjectList { get; set; }
+        public void DeserialiseRates(string projectListJsonString)
+        {
+            ProjectList = JsonConvert.DeserializeObject<ProjectListDTO>(projectListJsonString);
+        }
+    }
+}
