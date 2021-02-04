52acaef5ada6731536546fd1404523b257e55f1b
Jacob Stoneman (stonemaj) <jacob679@hotmail.co.uk>
2021-02-04 11:35:05 +0000

    GetActiveTaskById

--- APIMiniProject/APIMiniProject/Tasks/HTTPManager/TaskGetCallManager.cs
+++ APIMiniProject/APIMiniProject/Tasks/HTTPManager/TaskGetCallManager.cs
@@ -1,14 +1,42 @@
 ﻿using RestSharp;
+using System.Reflection;
 
 namespace APIMiniProject
 {
+	public class TaskParameter
+	{
+		public TaskParameter(long? projectID, long? labelID, string Filter, string Lang, long[] IDs)
+		{
+			project_id = projectID;
+			label_id = labelID;
+			filter = Filter;
+			lang = Lang;
+			ids = IDs;
+		}
+
+		long? project_id { get; set; } = null;
+		long? label_id { get; set; } = null;
+		string filter { get; set; } = null;
+		string lang { get; set; } = null;
+		long[] ids { get; set; } = null;
+	}
+
 	public class TaskGetCallManager : CallManager
 	{
 		public TaskGetCallManager(IRestClient _client) : base(_client) {}
 
-		//public IRestResponse GetActiveTaskByID(long id)
-		//{
+		public IRestResponse GetActiveTasksByID(TaskParameter parameters)
+		{
+			RestRequest request = new RestRequest(Method.GET);
+			request.Resource = $"tasks";
 
-		//}
+			foreach(PropertyInfo property in parameters.GetType().GetProperties())
+			{
+				if (property.GetValue(property.GetType()) != null) request.AddParameter(property.Name, property.GetValue(property.GetType()));
+			}
+
+			request.AddHeader("Authorization", $"Bearer {AppConfigReader.BearerToken}");
+			return ExecuteRequest(request);
+		}
 	}
 }
---
+++ APIMiniProject/APIMiniProject/Tasks/Services/TaskGetService.cs
@@ -0,0 +1,19 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIMiniProject
+{
+	public class TaskGetService
+	{
+		public TaskGetCallManager CallManager { get; set; } = new TaskGetCallManager(new RestClient(AppConfigReader.BaseUrl));
+		public void GetActiveTasksByID(long? projectID = null, long? labelID = null, string Filter = null, string Lang = null, long[] IDs = null)
+		{
+			TaskParameter newParameters = new TaskParameter(projectID, labelID, Filter, Lang, IDs);
+			CallManager.GetActiveTasksByID(newParameters);
+		}
+	}
+}
