8893af97a5ebda3c8a444f9dcdaff7a876f48dbe
Rob Bentley <robertbentley96@hotmail.co.uk>
2021-02-04 08:59:21 +0000

    Added edit project test file

---
+++ APIMiniProject/APIMiniProject/Tests/ProjectEditTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using RestSharp;
+
+namespace APIMiniProject
+{
+    public class ProjectEditTests
+    {
+        ProjectCreateService _createService = new ProjectCreateService(new ProjectCreateCallManager(new RestClient(AppConfigReader.BaseUrl)));
+
+        [Test]
+        public void PostCallSuccessfullyEditsName()
+        {
+            
+        }
+    }
+}
