2cfb2d12a1347db91770da17b14e5710bc992af5
Jacob Stoneman (stonemaj) <jacob679@hotmail.co.uk>
2021-02-04 12:38:56 +0000

    TaskGetService process results

--- APIMiniProject/APIMiniProject/Tasks/Services/TaskGetService.cs
+++ APIMiniProject/APIMiniProject/Tasks/Services/TaskGetService.cs
@@ -2,13 +2,20 @@
 
 namespace APIMiniProject
 {
-	public class TaskGetService
+	public class TaskGetService : ProjectService
 	{
 		public TaskGetCallManager CallManager { get; set; } = new TaskGetCallManager(new RestClient(AppConfigReader.BaseUrl));
+
+		public void ProcessResult(IRestResponse response)
+		{
+			SetStatus(response);
+			//if (StatusMessage == "OK") Result.DeserialiseProjectList(response.Content, isArray);
+		}
+
 		public void GetActiveTasksByID(long? projectID = null, long? labelID = null, string Filter = null, string Lang = null, long[] IDs = null)
 		{
 			TaskParameter newParameters = new TaskParameter(projectID, labelID, Filter, Lang, IDs);
-			CallManager.GetActiveTasksByID(newParameters);
+			ProcessResult(CallManager.GetActiveTasksByID(newParameters));
 		}
 	}
 }
