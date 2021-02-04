e3c7a2ba7a94937a06d7ed44329eeff7d606e31b
Sam Haimes <samhaimes@hotmail.com>
2021-02-04 12:17:48 +0000

    Tests for delete functionality

--- APIMiniProject/APIMiniProject/Tests/ProjectDeleteTests.cs
+++ APIMiniProject/APIMiniProject/Tests/ProjectDeleteTests.cs
@@ -5,20 +5,25 @@ namespace APIMiniProject
 {
     public class ProjectDeleteTests
     {
-            ProjectDeleteService _projectDeleteService = new ProjectDeleteService();
-            ProjectGetService _projectGetService = new ProjectGetService();
+        ProjectDeleteService _projectDeleteService = new ProjectDeleteService();
+        ProjectGetService _projectGetService = new ProjectGetService();
+        ProjectCreateService _createService = new ProjectCreateService(new ProjectCreateCallManager(new RestClient(AppConfigReader.BaseUrl)));
 
         [OneTimeSetUp]
         public void ProjectDeleteTestOneTimeSetup()
-        {
-            // create a test 
-            ProjectCreateService _createService = new ProjectCreateService(new ProjectCreateCallManager(new RestClient(AppConfigReader.BaseUrl)));
-            _createService.CreateProjectWithName("Test Delete Function");
-            if (_projectGetService.CallManager.GetProjectByID()
+        {      
+            _createService.CreateProject("Test Delete Function");
+            long id = _createService.ProjectDTO.Project.Id;
 
-
+            _projectDeleteService.DeleteProject(id);
         }
 
+        [Test]
+        public void TheProjectIsNoLongerThereByGetAllProjects()
+        {
+            _projectGetService.GetAllByName("Test Delete Function");
+            Assert.That(_projectGetService.Result.ProjectList.Length, Is.EqualTo(0)); //404: NotFound
+        }
 
         [Test]
         public void RequestSentOK()
@@ -27,16 +32,18 @@ namespace APIMiniProject
         }
 
         [Test]
-        public void TheTaskIsNoLongerThere()
+        public void TheProjectIsNoLongerThereByGetProject()
         {
-            Assert.That(_projectGetService.GetProjectByID(2257480369).Status, Is.EqualTo(404)); //404: NotFound
+            long id = _createService.ProjectDTO.Project.Id;
+            _projectGetService.GetProjectByID(id);
+            Assert.That(_projectGetService.StatusCode, Is.EqualTo(404)); //404: NotFound
 
         }
 
         [Test]
         public void DeleteTaskThatNeverExisted()
         { 
-           Assert.That(_projectDeleteService.CallManager.DeleteProject(123).StatusCode.ToString(), Is.EqualTo("400")); //404: BadRequest
+           Assert.That(_projectDeleteService.CallManager.DeleteProject(123).StatusCode.ToString(), Is.EqualTo("BadRequest")); //404: BadRequest
         }
 
     }
