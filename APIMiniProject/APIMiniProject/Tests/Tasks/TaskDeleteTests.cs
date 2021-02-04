ccb32c0b2038aab40f31abd32d5c0058f865ed51
Sam Haimes <samhaimes@hotmail.com>
2021-02-04 15:57:05 +0000

    running tests delete tasks

--- APIMiniProject/APIMiniProject/Tests/Tasks/TaskDeleteTests.cs
+++ APIMiniProject/APIMiniProject/Tests/Tasks/TaskDeleteTests.cs
@@ -7,27 +7,22 @@ namespace APIMiniProject
     {
         TaskDeleteService _taskDeleteService = new TaskDeleteService();
         TaskGetService _taskGetService = new TaskGetService();
-      //  TaskCreateService _taskCreateService = new TaskCreateService();
-        
+        private TaskCreateService _taskCreateService = new TaskCreateService(new TaskCreateCallManager(new RestClient(AppConfigReader.BaseUrl)));
 
         [OneTimeSetUp]
         public void TaskDeleteTestOneTimeSetup()
         {
 
-            // create task
-            //find task id 
-            // delete task
-
-            //_taskCreateService.CreateTask("Test Delete Task");
-            //long id = _taskCreateService.ProjectDTO.Project.Id;
-
-        //    _taskDeleteService.DeleteTask(id);
+            _taskCreateService.CreateTaskDueDateTime("Test Delete Task X");
+            long id = _taskCreateService.TaskDTO.Task.Id;
+            _taskDeleteService.DeleteTask(id);
         }
 
         [Test]
         public void RequestSentOK()
         {
-            _taskDeleteService.DeleteTask(4552300072);
+            long id = _taskCreateService.TaskDTO.Task.Id;
+            _taskDeleteService.DeleteTask(id);
             Assert.That(_taskDeleteService.Status, Is.EqualTo(204)); //204 : No Content
         }
 
@@ -35,28 +30,10 @@ namespace APIMiniProject
         [Test]
         public void DeleteTaskThatNeverExisted()
         {
-            Assert.That(_taskDeleteService.CallManager.DeleteTask(123).StatusCode.ToString(), Is.EqualTo("BadRequest")); //404 : BadRequest
+            Assert.That(_taskDeleteService.CallManager.DeleteTask(123).StatusCode.ToString(), Is.EqualTo("BadRequest")); //404: BadRequest
         }
 
-        [Ignore("waiting for get methods")]
-        [Test]
-        public void TheTaskIsNoLongerThereByGetAllTasks()
-        {
-            // get all by ??
-            _taskGetService.GetActiveTasks.
-            Assert.That(_taskGetService.Result.Tasks.Length, Is.EqualTo(0)); //404: NotFound
-        }
 
-        [Ignore("waiting for get methods")]
-        [Test]
-        public void TheTaskIsNoLongerThereByGetTask()
-        {
-            //test to check when you search id it doesnt show up
-
-            //long id = _taskcreateService.tasktDTO.Task.Id;
-            //_taskprojectGetService.GetProjectByID(id);
-            Assert.That(_taskGetService.StatusCode, Is.EqualTo(404)); //404: NotFound
-        }
 
     }
 }
