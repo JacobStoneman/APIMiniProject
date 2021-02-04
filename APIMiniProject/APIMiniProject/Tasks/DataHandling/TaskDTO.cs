a7dcc460beb222fec927ada74129e4ced9139787
zakkazmanli <zakariyyaakazmanli@gmail.com>
2021-02-04 13:51:50 +0000

    Add task model and DTO

---
+++ APIMiniProject/APIMiniProject/Tasks/DataHandling/TaskDTO.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIMiniProject
+{
+    public class TaskDTO
+    {
+        public TaskModel Task { get; set; }
+        public void DeserialiseTask(string taskJsonString)
+        {
+            Task = JsonConvert.DeserializeObject<TaskModel>(taskJsonString);
+        }
+    }
+}
