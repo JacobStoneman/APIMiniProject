3836152e324a5b0065a4c55a309e2c210592b0d1
Sam Haimes <samhaimes@hotmail.com>
2021-02-04 12:39:02 +0000

    added to class diagram

--- APIMiniProject/APIMiniProject/Services/Services/ProjectDeleteService.cs
+++ APIMiniProject/APIMiniProject/Services/Services/ProjectDeleteService.cs
@@ -2,16 +2,12 @@
 
 namespace APIMiniProject
 {
-	public class ProjectDeleteService
+	public class ProjectDeleteService : ProjectService
 	{
 		public ProjectDeleteCallManager CallManager { get; set; } = new ProjectDeleteCallManager(new RestClient(AppConfigReader.BaseUrl));
 		public ProjectDTO Result { get; set; } = new ProjectDTO();
 		public int Status { get; set; }
 
-		//public ProjectDeleteService(ProjectCrCallManager projectCreateCallManager)
-		//{
-		//	ProjectCreateCallManager = projectCreateCallManager;
-		//}
 		public void DeleteProject(long id)
 		{
 			IRestResponse response = CallManager.DeleteProject(id);
